eb53582cd97cca60f237165940dc829ce7d21e3f
ONE-LI <60172996+li199773@users.noreply.github.com>
2022-08-10 22:00:28 +0800

    Add files via upload

---
+++ shell-script/text4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "------$n------"
+echo '------$n------'
+echo $1
+echo $2
+echo "hello $1"
+echo '------$#------'
+echo $#
+echo '------$*------'
+echo $*
+echo '------$@------'
+echo $@
